b93e181f43e85ccb99ba73134014dbe5721ea137
SHAIKIDREES <86823800+SHAIKIDREES@users.noreply.github.com>
2022-12-16 09:59:19 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function setup() {
   canvas = createCanvas(300, 300);
-  canvas.center();
+  canvas.position(550,550);
   video = createCapture(VIDEO);
   video.hide();
   classifier=ml5.imageClassifier('MobileNet',modelLoaded);
@@ -21,15 +21,16 @@ function gotResult (error,results) {
 if (error){
   console.error(error);  
 } else{
-  if((results[0].confidence > 0.5) && (previous_result !=results[0].
-  label)){
+  if((results[0].confidence > 0.5) && (previous_result !=results[0].label)){
   
 console.log(results);
 previous_result=results[0].label;
-var synth=window.SpeechSynthesisUtterance(speak_data);
+var synth=window .speechSynthesis;
+speak_data="object detected is"+ results[0].label;
+var utterThis=window.SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 
-document.getElementById("result_object_name").innerHTML=results[0].label
+document.getElementById("result_object_name").innerHTML=results[0].label;
 document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
   }
 
